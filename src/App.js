872760827f81450987d367b3ec8372a3d4c7eaf4
Ian <ian.downey@gmail.com>
2023-07-27 21:40:44 +1000

    Added Analytics

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import PriceCard from "./components/PriceCard";
 import CalcCard from "./components/CalcCard";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const [usPrice, setUSPrice] = useState(0);
@@ -55,6 +56,7 @@ function App() {
   }, []);
 
   return (
+    
     <div className="App">
       <div class="header">
         <img
